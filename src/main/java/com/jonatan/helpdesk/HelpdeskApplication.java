4daa0b323155f591932d69ad2a32349176f03d71
Jonatan <jonatanreseck@yahoo.com.br>
2022-07-07 23:25:09 -0300

    criado perfil dev

--- src/main/java/com/jonatan/helpdesk/HelpdeskApplication.java
+++ src/main/java/com/jonatan/helpdesk/HelpdeskApplication.java
@@ -1,6 +1,5 @@
 package com.jonatan.helpdesk;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
