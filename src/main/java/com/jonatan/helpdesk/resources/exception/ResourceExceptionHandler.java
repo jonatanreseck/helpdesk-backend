d160c2d6fd8f9e11a13e3e354b93ed3970decaaa
Jonatan <jonatanreseck@yahoo.com.br>
2022-07-08 23:53:25 -0300

    adic. excessão para objeto não encontrado

---
+++ src/main/java/com/jonatan/helpdesk/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.jonatan.helpdesk.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.jonatan.helpdesk.services.Exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFoundException(ObjectNotFoundException ex,
+            HttpServletRequest request) {
+        StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),
+                "Object not Found", ex.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+}
---
+++ src/main/java/com/jonatan/helpdesk/services/Exceptions/ObjectNotFoundException.java
@@ -0,0 +1,14 @@
+package com.jonatan.helpdesk.services.Exceptions;
+
+public class ObjectNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+
+    public ObjectNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    
+}
