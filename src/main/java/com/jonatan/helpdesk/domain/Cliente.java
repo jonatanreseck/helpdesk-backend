8f918da7f644137ee8648a65b35c044cfdaaabaf
Jonatan <jonatanreseck@yahoo.com.br>
2022-07-11 21:36:46 -0300

    adic crud para cliente

--- src/main/java/com/jonatan/helpdesk/domain/Cliente.java
+++ src/main/java/com/jonatan/helpdesk/domain/Cliente.java
@@ -2,12 +2,14 @@ package com.jonatan.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.jonatan.helpdesk.domain.enums.Perfil;
+import com.jonatan.helpdesk.dtos.ClienteDto;
 
 @Entity
 public class Cliente extends Pessoa {
@@ -24,6 +26,18 @@ public class Cliente extends Pessoa {
 
     public Cliente(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
+        addPerfil(Perfil.CLIENTE);
+    }
+
+    public Cliente(ClienteDto obj) {
+        super();
+        this.id = obj.getId();
+        this.nome = obj.getNome();
+        this.cpf = obj.getCpf();
+        this.email = obj.getEmail();
+        this.senha = obj.getSenha();
+        this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+        this.dataCriacao = obj.getDataCriacao();
     }
 
     public List<Chamado> getChamados() {
--- src/main/java/com/jonatan/helpdesk/services/ClienteService.java
+++ src/main/java/com/jonatan/helpdesk/services/ClienteService.java
@@ -9,55 +9,55 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jonatan.helpdesk.domain.Pessoa;
-import com.jonatan.helpdesk.domain.Tecnico;
-import com.jonatan.helpdesk.dtos.TecnicoDto;
+import com.jonatan.helpdesk.domain.Cliente;
+import com.jonatan.helpdesk.dtos.ClienteDto;
 import com.jonatan.helpdesk.repositories.PessoaRepository;
-import com.jonatan.helpdesk.repositories.TecnicoRepository;
+import com.jonatan.helpdesk.repositories.ClienteRepository;
 import com.jonatan.helpdesk.services.Exceptions.DataIntegratyViolationException;
 import com.jonatan.helpdesk.services.Exceptions.ObjectNotFoundException;
 
 @Service
-public class TecnicoService {
+public class ClienteService {
     
     @Autowired
-    private TecnicoRepository repository;
+    private ClienteRepository repository;
 
     @Autowired
     private PessoaRepository pessoaRepository;
 
-    public Tecnico findById(Integer id){
-        Optional<Tecnico> obj = repository.findById(id);
+    public Cliente findById(Integer id){
+        Optional<Cliente> obj = repository.findById(id);
         return obj.orElseThrow(()->new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
     }
 
-    public List<Tecnico> findAll() {
+    public List<Cliente> findAll() {
         return repository.findAll();
     }
 
-    public Tecnico create(TecnicoDto objDto) {
+    public Cliente create(ClienteDto objDto) {
         objDto.setId(null);
         validaPorCpfEEmail(objDto);
-        Tecnico newObj = new Tecnico(objDto);
+        Cliente newObj = new Cliente(objDto);
         return repository.save(newObj);
     }
 
-    public Tecnico update(Integer id, @Valid TecnicoDto objDto) {
+    public Cliente update(Integer id, @Valid ClienteDto objDto) {
         objDto.setId(id);
-        Tecnico oldObj = findById(id);
+        Cliente oldObj = findById(id);
         validaPorCpfEEmail(objDto);
-        oldObj = new Tecnico(objDto);
+        oldObj = new Cliente(objDto);
         return repository.save(oldObj);
     }
 
     public void delete(Integer id) {
-        Tecnico obj = findById(id);
+        Cliente obj = findById(id);
         if(obj.getChamados().size()>0){
             throw new DataIntegratyViolationException("Técnico possui ordens de serviço e não pode ser deletado!");
         }
         repository.deleteById(id);
     }
 
-    private void validaPorCpfEEmail(TecnicoDto objDto) {
+    private void validaPorCpfEEmail(ClienteDto objDto) {
         Optional<Pessoa> obj = pessoaRepository.findByCpf(objDto.getCpf());
         if(obj.isPresent() && obj.get().getId() != objDto.getId()){
             throw new DataIntegratyViolationException("CPF já cadastrado no sistema!");
