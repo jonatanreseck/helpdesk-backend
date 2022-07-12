9e4910a4b81a6f1c426b3bfceb88fc1a85506791
Jonatan <jonatanreseck@yahoo.com.br>
2022-07-11 22:01:54 -0300

    adic. end point findbyid para chamados

---
+++ src/main/java/com/jonatan/helpdesk/dtos/ChamadoDTO.java
@@ -0,0 +1,131 @@
+package com.jonatan.helpdesk.dtos;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.jonatan.helpdesk.domain.Chamado;
+
+public class ChamadoDTO {
+   
+    private Integer id;
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dataAbertura = LocalDate.now();
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dataFechamento;
+    private Integer prioridade;
+    private Integer status;
+    private String titulo;
+    private String obeservacoes;
+    private Integer tecnico;
+    private Integer cliente;
+    private String nomeTecnico;
+    private String nomeCliente;
+
+    public ChamadoDTO() {
+        super();
+    }
+
+    public ChamadoDTO(Chamado obj) {
+        super();
+        this.id = obj.getId();
+        this.dataAbertura = obj.getDataAbertura();
+        this.dataFechamento = obj.getDataFechamento();
+        this.prioridade = obj.getPrioridade().getCodigo();
+        this.status = obj.getStatus().getCodigo();
+        this.titulo = obj.getTitulo();
+        this.obeservacoes = obj.getObeservacoes();
+        this.tecnico = obj.getTecnico().getId();
+        this.cliente = obj.getCliente().getId();
+        this.nomeTecnico = obj.getTecnico().getNome();
+        this.nomeCliente = obj.getCliente().getNome();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDate getDataAbertura() {
+        return dataAbertura;
+    }
+
+    public void setDataAbertura(LocalDate dataAbertura) {
+        this.dataAbertura = dataAbertura;
+    }
+
+    public LocalDate getDataFechamento() {
+        return dataFechamento;
+    }
+
+    public void setDataFechamento(LocalDate dataFechamento) {
+        this.dataFechamento = dataFechamento;
+    }
+
+    public Integer getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(Integer prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getObeservacoes() {
+        return obeservacoes;
+    }
+
+    public void setObeservacoes(String obeservacoes) {
+        this.obeservacoes = obeservacoes;
+    }
+
+    public Integer getTecnico() {
+        return tecnico;
+    }
+
+    public void setTecnico(Integer tecnico) {
+        this.tecnico = tecnico;
+    }
+
+    public Integer getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Integer cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getNomeTecnico() {
+        return nomeTecnico;
+    }
+
+    public void setNomeTecnico(String nomeTecnico) {
+        this.nomeTecnico = nomeTecnico;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+    
+}
---
+++ src/main/java/com/jonatan/helpdesk/services/ChamadoService.java
@@ -0,0 +1,22 @@
+package com.jonatan.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jonatan.helpdesk.domain.Chamado;
+import com.jonatan.helpdesk.repositories.ChamadoRepository;
+import com.jonatan.helpdesk.services.Exceptions.ObjectNotFoundException;
+
+@Service
+public class ChamadoService {
+    
+    @Autowired
+    private ChamadoRepository repository;
+
+    public Chamado findById(Integer id){
+        Optional<Chamado> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id));
+    }
+}
