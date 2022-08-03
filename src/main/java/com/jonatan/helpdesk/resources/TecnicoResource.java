604d0824470824ef5c9dda7e82ec060eec14e49e
jonat <jonat@DESKTOP-D1BKU6U>
2022-08-03 00:21:17 -0300

    aula 25: jwt aparentemente funcionando
    pausa na aula para testar se as alterações e correções surtiram efeito

--- src/main/java/com/jonatan/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/jonatan/helpdesk/resources/TecnicoResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -42,6 +43,7 @@ public class TecnicoResource {
         return ResponseEntity.ok(listDto);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PostMapping
     public ResponseEntity<TecnicoDto> create(@Valid @RequestBody TecnicoDto objDto){
         Tecnico newObj = service.create(objDto);
@@ -49,12 +51,14 @@ public class TecnicoResource {
         return ResponseEntity.created(uri).build();
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<TecnicoDto> update(@PathVariable Integer id,@Valid @RequestBody TecnicoDto objDto){
         Tecnico obj = service.update(id, objDto);
         return ResponseEntity.ok().body(new TecnicoDto(obj));
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<TecnicoDto> delete(@PathVariable Integer id){
         service.delete(id);
